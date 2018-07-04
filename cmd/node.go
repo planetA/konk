be6dddd10383652783ee3d7a9ff028c3874d62e3
Maksym Planeta <mplaneta@os.inf.tu-dresden.de>
2018-07-04 14:53:44 +0200

    Change init command

--- cmd/node.go
+++ cmd/node.go
@@ -1,20 +1,49 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 
 	"github.com/planetA/konk/docs"
+	"github.com/planetA/konk/pkg/node"
+)
+
+var (
+	// Unique Id of a node in a network
+	nodeId int
 )
 
 var nodeCmd = &cobra.Command{
 	TraverseChildren: true,
-	Use:   docs.NodeUse,
-	Short: docs.NodeShort,
-	Long:  docs.NodeLong,
+	Use:              docs.NodeUse,
+	Short:            docs.NodeShort,
+	Long:             docs.NodeLong,
 	Run: func(cmd *cobra.Command, args []string) {
 	},
 }
 
+var nodeInitCmd = &cobra.Command{
+	Use:   docs.NodeInitUse,
+	Short: docs.NodeInitShort,
+	Long:  docs.NodeInitLong,
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		if cmd.Flags().Changed("id") {
+			return fmt.Errorf(`Don't update "id" for now`)
+		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		node.Init(nodeId)
+	},
+}
+
 func init() {
+	nodeInitCmd.Flags().IntVarP(&nodeId, "id", "i", 0, "Node id")
+	nodeCmd.AddCommand(nodeInitCmd)
+
 	KonkCmd.AddCommand(nodeCmd)
+}
+
+func init() {
 }

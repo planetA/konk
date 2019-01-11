2d7d228acc0451764e1f2882d26865a5e9c9f335
Maksym Planeta <mplaneta@os.inf.tu-dresden.de>
2019-01-11 18:15:59 +0100

    Protect array with containers with a mutex

--- srv/nymph/server.go
+++ srv/nymph/server.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"strconv"
+	"sync"
 
 	"github.com/planetA/konk/pkg/container"
 	"github.com/planetA/konk/pkg/coordinator"
@@ -16,8 +17,9 @@ import (
 
 // Type for the server state of the connection to a nymph daemon
 type Nymph struct {
-	reaper     *Reaper
-	containers map[container.Id]*container.Container
+	reaper         *Reaper
+	containerMutex *sync.Mutex
+	containers     map[container.Id]*container.Container
 }
 
 func NewNymph() (*Nymph, error) {
@@ -27,8 +29,9 @@ func NewNymph() (*Nymph, error) {
 	}
 
 	return &Nymph{
-		reaper:     reaper,
-		containers: make(map[container.Id]*container.Container),
+		reaper:         reaper,
+		containerMutex: &sync.Mutex{},
+		containers:     make(map[container.Id]*container.Container),
 	}, nil
 }
 
@@ -53,7 +56,9 @@ func (n *Nymph) PrepareReceive(args *ReceiveArgs, reply *int) error {
 			log.Panicf("Connection failed: %v", err)
 		}
 
+		n.containerMutex.Lock()
 		n.containers[cont.Id] = cont
+		n.containerMutex.Unlock()
 	}()
 	return nil
 }
@@ -63,7 +68,9 @@ func (n *Nymph) Send(args *SendArgs, reply *bool) error {
 	log.Println("Received a request to send a checkpoint to ", args.Host, args.Port)
 
 	address := net.JoinHostPort(args.Host, strconv.Itoa(args.Port))
+	n.containerMutex.Lock()
 	container := n.containers[args.ContainerId]
+	n.containerMutex.Unlock()
 	if err := criu.Migrate(container, address); err != nil {
 		*reply = false
 		return err
@@ -83,7 +90,9 @@ func (n *Nymph) CreateContainer(args CreateContainerArgs, path *string) error {
 	}
 
 	// Remember the container object
+	n.containerMutex.Lock()
 	n.containers[args.Id] = cont
+	n.containerMutex.Unlock()
 
 	cont.Network, err = container.NewNetwork(cont.Id, cont.Path)
 	if err != nil {
@@ -100,7 +109,9 @@ func (n *Nymph) CreateContainer(args CreateContainerArgs, path *string) error {
 func (n *Nymph) NotifyProcess(args NotifyProcessArgs, reply *bool) error {
 	var err error
 
+	n.containerMutex.Lock()
 	err = n.containers[args.Id].Notify()
+	n.containerMutex.Unlock()
 	if err != nil {
 		return fmt.Errorf("Notifying the init process failed: %v", err)
 	}
@@ -114,9 +125,11 @@ func (n *Nymph) NotifyProcess(args NotifyProcessArgs, reply *bool) error {
 }
 
 func (n *Nymph) _Close() {
+	n.containerMutex.Lock()
 	for _, cont := range n.containers {
 		cont.Close()
 	}
+	n.containerMutex.Unlock()
 
 	n.reaper.Close()
 }

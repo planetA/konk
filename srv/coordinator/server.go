302cd9fb40b9f903d6725c93258092b5e719b0f1
Maksym Planeta <mplaneta@os.inf.tu-dresden.de>
2019-01-12 02:00:04 +0100

    Add mutex to coordinator DB

--- srv/coordinator/server.go
+++ srv/coordinator/server.go
@@ -3,6 +3,7 @@ package coordinator
 import (
 	"fmt"
 	"log"
+	"sync"
 
 	"github.com/planetA/konk/pkg/container"
 
@@ -14,19 +15,23 @@ type Location struct {
 }
 
 type Coordinator struct {
+	locationMutex *sync.Mutex
 	locationDB map[container.Id]Location
 }
 
 func NewCoordinator() *Coordinator {
 	return &Coordinator{
+		locationMutex: &sync.Mutex{},
 		locationDB: make(map[container.Id]Location),
 	}
 }
 
 func (c *Coordinator) RegisterContainer(args *RegisterContainerArgs, reply *bool) error {
+	c.locationMutex.Lock()
 	c.locationDB[args.Id] = Location{args.Hostname}
 
 	log.Println(c.locationDB)
+	c.locationMutex.Unlock()
 
 	*reply = true
 	return nil
@@ -36,7 +41,9 @@ func (c *Coordinator) RegisterContainer(args *RegisterContainerArgs, reply *bool
 func (c *Coordinator) Migrate(args *MigrateArgs, reply *bool) error {
 	log.Printf("Received a request to move rank %v to %v\n", args.Id, args.DestHost)
 
+	c.locationMutex.Lock()
 	src := c.locationDB[args.Id]
+	c.locationMutex.Unlock()
 
 	if err := Migrate(args.Id, src.Hostname, args.DestHost); err != nil {
 		*reply = false
